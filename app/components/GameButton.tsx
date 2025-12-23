460f3ea097f0c8986d533e775a4df5c6705361bd
git-likhon <arefinriad40@gmail.com>
2025-12-23 20:10:24 +0600

    uid-change

--- app/components/GameButton.tsx
+++ app/components/GameButton.tsx
@@ -11,7 +11,7 @@ function LaunchGameButton({ userId, walletAmount, gameUid }: { userId: string; w
       },
       body: JSON.stringify({
         userName: "player123",
-        game_uid: "e3f45e2f03388056aba660b7e737ecf0",
+        game_uid: "6dcaf78e4e23929cbe2deb3d1210928c",
         credit_amount: 1200
       })
     });
