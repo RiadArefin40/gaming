93fa9f2f2e6b5652f16c1b59be1ed15ecdf01c94
git-likhon <arefinriad40@gmail.com>
2025-12-29 00:35:02 +0600

    ok

--- app/components/SafeImageProps.tsx
+++ app/components/SafeImageProps.tsx
@@ -49,7 +49,7 @@ export default function SafeImage({
     <img
     src={imgSrc}
     alt={alt}
-    className= "h-8 rounded-full w-8 opacity-80 z-200 absolute top-0 right-2"
+    className= "h-8 rounded-full w-8 opacity-80 absolute top-0 right-1"
     onLoad={() => setLoading(false)}
     onError={() => {
       setImgSrc(FALLBACK_IMAGE);
