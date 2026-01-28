5a2c3e7b19df9f242343c2b79cebf713f135922d
git-likhon <arefinriad40@gmail.com>
2026-01-28 14:54:05 +0600

    ok

--- app/components/SafeImageProps.tsx
+++ app/components/SafeImageProps.tsx
@@ -34,7 +34,7 @@ export default function SafeImage({
     <img
       src={imgSrc}
       alt={alt}
-      className={`object-contain  transition-opacity duration-500 rounded-md ${
+      className={`object-contain h-[160px] transition-opacity duration-500 rounded-md ${
         loading ? "opacity-0" : "opacity-100"
       }`}
       onLoad={() => setLoading(false)}
