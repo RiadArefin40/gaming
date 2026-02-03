a60ea448985f5b7e3fe0e792475345d9d2e2df5e
git-likhon <arefinriad40@gmail.com>
2026-02-03 20:44:08 +0600

    ok

--- app/components/MobileAppBar.tsx
+++ app/components/MobileAppBar.tsx
@@ -120,7 +120,7 @@ export default function MobileAppBar() {
     {
       name: "Exclusive",
       icon: <Crown className="w-5 h-5 mr-1" />,
-      children: <ExclusiveGrid items={ex} />,
+      children: <ExclusiveGrid items={ex} cat={"Exclusive"} />,
     },
 
     {
