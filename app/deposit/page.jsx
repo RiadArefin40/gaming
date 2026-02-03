54d81244bf8bd22959472c94406fbd80114e9850
git-likhon <arefinriad40@gmail.com>
2026-02-03 21:16:53 +0600

    ok

--- app/deposit/page.jsx
+++ app/deposit/page.jsx
@@ -749,7 +749,7 @@ useEffect(() => {
                   onClick={() => copyText(receiverNumber)}
                   disabled={!receiverNumber}
                 >
-                  <span className="text-xl text-slate-600">
+                  <span className="text-xl text-slate-800">
                     {copied ? "✔" : "Copy"}
                   </span>
                 </Button>
