58a27841393a8ed10a7b51ec04caff595df9cd16
git-likhon <arefinriad40@gmail.com>
2026-02-17 21:15:21 +0600

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,7 +72,7 @@ export const viewport = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="!my-[20px]">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black-800 !py-[10px]`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased bg-black-800 `}>
         <SidebarProvider>
           <div className="flex h-full w-full">
             <AppSidebar />
